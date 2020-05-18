cc04171f94239776ea1f785a8cf4cfc9545f182c
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-05-18 20:55:38 +0700

    fix top menu

--- core/assets/js/core/main/_global.js
+++ core/assets/js/core/main/_global.js
@@ -4,6 +4,7 @@ var video = document.querySelectorAll('video');
 var openNavBtn = document.querySelector("#navBtn");
 var navbar = document.querySelector('.navbar');
 var closeNavBtn = document.querySelector('#navbarClose');
+var htmlWrapper = document.querySelector('.wrapper');
 // auto LazyLoad img and video
 function iframeResposive(){
   for (i = 0; i < iframe.length; i++) {
@@ -27,7 +28,9 @@ var lazyLoadInstance = new LazyLoad({elements_selector: ".lazy"});
 
 openNavBtn ? openNavBtn.onclick = function(){
   navbar.classList.add('active');
+  htmlWrapper.classList.add('active');
 }:{};
 closeNavBtn ? closeNavBtn.onclick = function(){
     navbar.classList.remove('active');
+    htmlWrapper.classList.remove('active');
 }:{};
