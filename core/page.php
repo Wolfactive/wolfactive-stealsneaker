7175c6483c3ccf1eec7bf5a9734617cc0dd40372
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-05-20 07:06:16 +0700

    fix page

--- core/page.php
+++ core/page.php
@@ -3,10 +3,7 @@ get_header();
 get_template_part('sections/breadcums');
 ?>
 <div class="page__contain my-60">
-  <div class="container">
-    <h1 class="title--section text--upcase text--center">
-      <?php the_title() ?>
-    </h1>
+  <div class="container">    
     <div class="page__content my-40">
       <?php the_content(); ?>
     </div>
