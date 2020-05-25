a43dac05fe7fa3df8c2ab1eb0019fada4bde3b0c
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-05-25 23:19:07 +0700

    crop img

--- core/functions.php
+++ core/functions.php
@@ -635,3 +635,17 @@ function get_term_list_check($taxonamy_slug,$name_tag){
       $countTerm++;
       }
   }
+  // Setting hình crop hình đại diện
+  function hk_get_thumb($id, $w, $h){
+  	if(get_post_thumbnail_id($id)){
+  		$url = wp_get_attachment_url( get_post_thumbnail_id($id));
+  	} else {
+  		$url = get_bloginfo('template_directory').'/no-thumb.jpg';
+  	}
+  	$image = huykira_image_resize($url, $w, $h, true, false);
+  	return $image['url'];
+  }
+  function hk_get_image($url, $w, $h){
+  	$image = huykira_image_resize($url, $w, $h, true, false);
+  	return $image['url'];
+  }
