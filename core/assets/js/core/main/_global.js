0c3f3eababb79dce2cfa0d99fc76e067d0dcaf41
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-05-27 16:13:57 +0700

    fix filter

--- core/assets/js/core/main/_global.js
+++ core/assets/js/core/main/_global.js
@@ -89,7 +89,7 @@ function actionFilter(arrayFilter,arrayLabel) {
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = "expires="+d.toUTCString();
+  var expires = "expires="+d.toUTCString(); 
   document.cookie = cname + "=" + cvalue + ";" + expires +";path=/loc-san-pham/";
 }
 /* create cookie to filter*/
