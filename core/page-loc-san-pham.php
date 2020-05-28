beaae634e3a15ac51a746744e13c9d7db201f5b5
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-05-28 19:53:35 +0700

    fix responsive

--- core/page-loc-san-pham.php
+++ core/page-loc-san-pham.php
@@ -141,10 +141,10 @@ get_template_part('sections/breadcums');
          <?php endif;?>
          <div class="lastest__config">
            <div class="lastest__config-btn">
-             <button class="btn" type="button" aria-label="view-quick" data-view="<?php the_title(); ?>" name="viewQuick" id="viewQuick">Xem nhanh</button>
+             <button class="btn" type="button" aria-label="view-quick" data-view="<?php the_title(); ?>" name="viewQuick">Xem nhanh</button>
            </div>
            <div class="lastest__config-btn">
-             <a href="<?php the_permalink(); ?>" class="btn" id="viewDetail">Xem chi tiết</a>
+             <a href="<?php the_permalink(); ?>" class="btn">Xem chi tiết</a>
            </div>
          </div>
         </div>
