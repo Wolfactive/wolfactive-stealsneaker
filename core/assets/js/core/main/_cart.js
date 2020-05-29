d16ebac206f5291b81b3c203949c53f08c49db05
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-05-29 11:28:18 +0700

    fix cart transform

--- core/assets/js/core/main/_cart.js
+++ core/assets/js/core/main/_cart.js
@@ -14,14 +14,16 @@ function LayLocalStorage() {
     if (!jsonData) { localStorage = []; return;}
     productBuyArray = JSON.parse(jsonData);
 }
+function pushToArray(){
+  productBuyArray.push(productBuy);
+  productBuyArrayPush = [].concat(_toConsumableArray(productBuyArrayPush), _toConsumableArray(productBuyArray));
+}
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 function get_cart_item(tenSP,giaSP,giaKMSP,sizeSP,soLuongSP){
   var productBuy = new sanPham(tenSP,giaSP,giaKMSP,sizeSP,soLuongSP);
   var productBuyArray = [];
   productBuyArray.length !== 0 ? productBuyArray.forEach(function(item, i){
-    productBuy.tenSP === item.tenSP ? item.soLuongSP + productBuy.soLuongSP : productBuyArray.push(productBuy);
-  }) :   productBuyArray.push(productBuy);
-  function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-productBuyArrayPush = [].concat(_toConsumableArray(productBuyArrayPush), _toConsumableArray(productBuyArray));
+    productBuy.tenSP === item.tenSP ? item.soLuongSP + productBuy.soLuongSP : pushToArray();
+  }) :   pushToArray();
   LuuVaoLocalStorage(productBuyArrayPush);
 }
